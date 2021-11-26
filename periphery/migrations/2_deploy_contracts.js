a9d5bbd9d94aeb24838420597ee30a8827db457d
arthur.gallina@gmail.com <arthur.gallina@gmail.com>
2021-11-26 10:22:31 -0300

    comment unused

--- periphery/migrations/2_deploy_contracts.js
+++ periphery/migrations/2_deploy_contracts.js
@@ -1,5 +1,15 @@
-const Migrations = artifacts.require("Migrations");
+const Router = artifacts.require("UniswapV2Router02");
+const WETH = artifacts.require("WETH");
 
-module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+module.exports = async function (deployer, network, addresses) {
+    let weth;
+    const FACTORY_ADDRESS = '0x154Ba06218C6B5E443C65de0D9333D21a1788bb3';
+    if(network === 'mainnet') {
+        weth = await WETH.at('0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2');
+    } else {
+        await deployer.deploy(WETH);
+        weth = await WETH.deployed();
+    }
+
+  await deployer.deploy(Router, FACTORY_ADDRESS, weth.address);
 };
